3032ee4b73fa602f1ad39915071f29bdff217a65
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-03-29 16:06:40 +0100

    affichage de premier question à partir de Js sur notre fichier index.html

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ btnRun.addEventListener("click", RunApp);
 
 function RunApp() {
   section.textContent = "";
+
+  const bloc1 = document.createElement("div");
+  const paraBloc1 = document.createElement("p");
+
+  //Tableau aux questions
+  const questions = [
+    "Etablissez la différence entre la surdéfinition et la redéfinition de la méthode",
+    "Dans la Programmation Orientée Objet quelle est l'importance de la signature dans la surcharge des méthodes",
+    "Explique-moi le concept classe abstraite en s'appuyant d'un exemple concret",
+  ];
+  //On genere un nombre aléatoire en se basant sur la taille du tableau
+  const indexeAleatoire = Math.floor(Math.random() * 3);
+  let viewQuestion = questions[indexeAleatoire]; //le nombre aléatoire obtenue est passé comme indexe au tableau
+  paraBloc1.textContent = viewQuestion; //on affiche la question obtenue aléatoirement dans le tableau
+
+  bloc1.appendChild(paraBloc1);
+  section.appendChild(bloc1);
 }
