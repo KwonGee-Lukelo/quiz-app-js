f2e7abacdc92b3194bb404e793b1ec95bbb74412
KwonGee-Lukelo <wahabfall17@gmail.com>
2025-04-01 22:16:16 +0200

    Création des nouvelles balises et l'ajout des classes

--- app.js
+++ app.js
@@ -12,17 +12,21 @@ function RunApp() {
   const paraBloc1 = document.createElement("p");
   const btnCheck = document.createElement("i");
   const tableBoard = document.createElement("h2");
+  let score = document.createElement("p");
+  const btnYes = document.createElement("button");
+  const btnNo = document.createElement("button");
+  const btnNext = document.createElement("button");
 
   //Tableau aux questions
   const questions = [
-    "En POO,  le ramasse-miettes(Garbage Collector) libère automatiquement la mémoire occupée par les objets inutilisés",
-    "Un destructeur est appelé automatiquement avant la suppression d'un objet",
-    "La programmation orientée objet est un paradigme basé sur les objets",
+    "En POO,  le ramasse-miettes(Garbage Collector) libère automatiquement la mémoire occupée par les objets inutilisés ?",
+    "Un destructeur est appelé automatiquement avant la suppression d'un objet ?",
+    "La programmation orientée objet est un paradigme basé sur les objets ?",
     "Un objet est une instance d'une classe",
-    "Une classe est un plan ou un modèle pour créer les objets",
-    "La POO ne peut être utilisée qu'avec des langages comme Java et C++",
-    "Les concepts fondamentaux de la POO sont l'encapsulation, l'héritage et le polymorphisme",
-    "Un objet peut exister sans une classe",
+    "Une classe est un plan ou un modèle pour créer les objets ?",
+    "La POO ne peut être utilisée qu'avec des langages comme Java et C++ ?",
+    "Les concepts fondamentaux de la POO sont l'encapsulation, l'héritage et le polymorphisme ?",
+    "Un objet peut exister sans une classe ?",
     "Une classe peut contenir des attributs et des méthodes ?",
     "Une classe peut avoir plusieurs instances ?",
     "Tous les objets d'une même classe partagent le même état ?",
@@ -44,15 +48,26 @@ function RunApp() {
 
   paraBloc1.textContent = viewQuestion; //on affiche la question obtenue aléatoirement dans le tableau
 
-  //Tableau aux reponses
-  const reponses = [];
-
   tableBoard.textContent = 0;
-  tableBoard.insertAdjacentElement("beforebegin", bloc3);
+  score.textContent = "SCORE : ";
+  btnYes.textContent = "Oui";
+  btnNo.textContent = "Non";
+  btnNext.textContent = "Passer";
+
+  //Ajout de class css
+  bloc2.classList.add("bloc2");
+  btnYes.classList.add("btn__yes");
+  btnNo.classList.add("btn__no");
+  btnNext.classList.add("btn__next");
+  bloc3.classList.add("bloc3");
 
   bloc1.appendChild(paraBloc1);
+  bloc2.appendChild(btnYes);
+  bloc2.appendChild(btnNo);
+  bloc2.appendChild(btnNext);
+  bloc3.appendChild(score);
   bloc3.appendChild(tableBoard);
+  section.appendChild(bloc3);
   section.appendChild(bloc1);
   section.appendChild(bloc2);
-  section.appendChild(bloc3);
 }
